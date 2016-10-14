72ae996fec0ed4b1dce5a64030b110e19d3d50fa
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-10-13 22:08:38 -0400

    new assets added.  Day, hour, and minute counter logs time spent in space before collision.  Asteroid counter tracks number of asteroids dodged.  Asteroid count and time stop after satellite collides with an asteroid

--- NewHorizons/NewHorizons/GameScene.swift
+++ NewHorizons/NewHorizons/GameScene.swift
@@ -11,6 +11,22 @@ import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate
 {
+    var missionDurationLabel = SKLabelNode(text: "Mission Duration")
+    var missionMinuteLabel = SKLabelNode(text: "Minute")
+    var missionHourLabel = SKLabelNode(text: "Hour")
+    var missionDayLabel = SKLabelNode(text: "Day")
+    var day = 0
+    var hour = 0
+    var minute = 0
+    var dayTimeLabel = SKLabelNode(text: "")
+    var hourTimeLabel = SKLabelNode(text: "")
+    var minuteTimeLabel = SKLabelNode(text: "")
+    
+    var dodgedAsteroids = 0
+    var asteroidsDodgedCountLabel = SKLabelNode(text: "0")
+    var asteroidsDodgedLabel = SKLabelNode(text: "Asteroids dodged")
+    var asteroidsDodgedImage = SKSpriteNode(imageNamed: "AsteroidsDodged")
+    
     enum bitMask: UInt32
     {
         case satellite = 1
@@ -36,18 +52,73 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         
         addChild(satellite)
         
-        run(SKAction.repeatForever(
-            SKAction.sequence([
-                SKAction.run(spawnAsteroid),
-                SKAction.wait(forDuration: 1.0)
-                ])
-        ))
+        run(SKAction.repeatForever(SKAction.sequence([
+            SKAction.run(spawnAsteroid),
+            SKAction.wait(forDuration: 1.0)
+            ])), withKey: "spawnAsteroid")
         
         physicsWorld.contactDelegate = self
         physicsWorld.gravity = CGVector(dx: 0.0, dy: -0.9)
         
         satellite.physicsBody?.contactTestBitMask = bitMask.asteroid.rawValue
         satellite.physicsBody?.collisionBitMask = bitMask.frame.rawValue
+        
+        missionDurationLabel.fontSize = 30
+        missionDurationLabel.fontName = "Helvetica Neue"
+        missionDurationLabel.fontColor = SKColor.red
+        missionDurationLabel.position = CGPoint(x: frame.size.width / 2, y: frame.size.height - 45)
+        addChild(missionDurationLabel)
+        
+        missionMinuteLabel.fontSize = 30
+        missionMinuteLabel.fontName = "Helvetica Neue"
+        missionMinuteLabel.position = CGPoint(x: frame.size.width / 2 + 150, y: frame.size.height - 85)
+        addChild(missionMinuteLabel)
+        
+        missionHourLabel.fontSize = 30
+        missionHourLabel.fontName = "Helvetica Neue"
+        missionHourLabel.position = CGPoint(x: frame.size.width / 2, y: frame.size.height - 85)
+        addChild(missionHourLabel)
+        
+        missionDayLabel.fontSize = 30
+        missionDayLabel.fontName = "Helvetica Neue"
+        missionDayLabel.position = CGPoint(x: frame.size.width / 2 - 150, y: frame.size.height - 85)
+        addChild(missionDayLabel)
+        
+        minuteTimeLabel.fontSize = 30
+        minuteTimeLabel.fontName = "Helvetica Neue"
+        minuteTimeLabel.position = CGPoint(x: frame.size.width / 2 + 150, y: frame.size.height - 120)
+        addChild(minuteTimeLabel)
+        
+        hourTimeLabel.fontSize = 30
+        hourTimeLabel.fontName = "Helvetica Neue"
+        hourTimeLabel.position = CGPoint(x: frame.size.width / 2, y: frame.size.height - 120)
+        addChild(hourTimeLabel)
+        
+        dayTimeLabel.fontSize = 30
+        dayTimeLabel.fontName = "Helvetica Neue"
+        dayTimeLabel.position = CGPoint(x: frame.size.width / 2 - 150, y: frame.size.height - 120)
+        addChild(dayTimeLabel)
+        
+        asteroidsDodgedImage.position = CGPoint(x: frame.size.width / 5.7, y: frame.size.height - 350)
+        addChild(asteroidsDodgedImage)
+        
+        asteroidsDodgedLabel.fontSize = 28
+        asteroidsDodgedLabel.fontName = "Helvetica Neue"
+        asteroidsDodgedLabel.position = CGPoint(x: frame.size.width / 4.7, y: frame.size.height - 400)
+        asteroidsDodgedLabel.zPosition = 1
+        addChild(asteroidsDodgedLabel)
+        
+        asteroidsDodgedCountLabel.fontSize = 30
+        asteroidsDodgedCountLabel.fontName = "Helvetica Neue"
+        asteroidsDodgedCountLabel.position = CGPoint(x: frame.size.width / 15.5, y: frame.size.height - 350)
+        asteroidsDodgedCountLabel.zPosition = 1
+        addChild(asteroidsDodgedCountLabel)
+        
+        
+        let updateMissionTimeLabels = SKAction.run(updateMissionTime)
+        let updateTime = SKAction.wait(forDuration: 1.0)
+        let updateSequence = SKAction.sequence([updateMissionTimeLabels, updateTime])
+        run(SKAction.repeatForever(updateSequence), withKey: "missionDurationTime")
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
@@ -74,6 +145,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         asteroid.physicsBody?.categoryBitMask = bitMask.asteroid.rawValue
         asteroid.physicsBody?.contactTestBitMask = bitMask.satellite.rawValue
         
+        asteroid.name = "Asteroid"
         addChild(asteroid)
     }
     
@@ -89,8 +161,53 @@ class GameScene: SKScene, SKPhysicsContactDelegate
             let firstNode = contact.bodyA.node
             firstNode?.physicsBody?.allowsRotation = true
             firstNode?.removeFromParent()
+            
+            removeAction(forKey: "missionDurationTime")
+            removeAction(forKey: "spawnAsteroid")
+            
+            enumerateChildNodes(withName: "Asteroid", using: { (asteroid, _) in
+                asteroid.name = "dontCountMe"
+            })
+            
         default:
             return
         }
+    }
+    
+    func updateMissionTime()
+    {
+        minute += 1
+        
+        if minute == 60
+        {
+            hour += 1
+            minute = 0
+        }
+        
+        if hour == 60
+        {
+            day += 1
+            hour = 0
+        }
+        
+        minuteTimeLabel.text = "\(minute)"
+        hourTimeLabel.text = "\(hour)"
+        dayTimeLabel.text = "\(day)"
+        
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval)
+    {
+        enumerateChildNodes(withName: "Asteroid") { asteroid,_ in
+            
+            if asteroid.position.y <= 3
+            {
+                self.dodgedAsteroids += 1
+                self.asteroidsDodgedCountLabel.text = (text: "\(self.dodgedAsteroids)")
+                asteroid.removeFromParent()
+            }
+        }
+        
     }
 }
